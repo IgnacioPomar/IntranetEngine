83b66d516b5f37e20fe88b0f07ddeeae3aa19551
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2022-03-04 14:50:28 +0100

    Solved installation bug: change of name of moduleAuth

--- src/rsc/default/site_cfg_def.php
+++ src/rsc/default/site_cfg_def.php
@@ -7,7 +7,7 @@
 $GLOBALS ['Version'] = '0.1';
 
 // Selected modules
-$GLOBALS ['authModule'] = 'src/auth_vanilla.php';
+$GLOBALS ['moduleAuth'] = 'src/auth_vanilla.php';
 $GLOBALS ['moduleMenu'] = 'src/menu_json.php';
 
 // ----------------------------------
