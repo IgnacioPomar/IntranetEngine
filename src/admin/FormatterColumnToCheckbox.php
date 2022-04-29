d2f2deec383a015c4e8238b3d9640d39804167d3
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2022-04-29 23:30:03 +0200

    Remove duplicated code

---
+++ src/admin/FormatterColumnToCheckbox.php
@@ -0,0 +1,27 @@
+<?php
+
+class FormatterColumnToCheckbox
+{
+
+
+	/**
+	 *
+	 * @param mixed $val
+	 * @param string $class
+	 * @return string
+	 */
+	public function getSpan ($val, $class)
+	{
+		$retVal = "<div class='$class'>";
+
+		$attributes = [ 'disabled'];
+		if ($val)
+		{
+			$attributes [] = 'checked';
+		}
+
+		$retVal .= '<input type="checkbox"' . join (' ', $attributes) . '>';
+		$retVal .= '</div>';
+		return $retVal;
+	}
+}
--- src/admin/MaintenanceGroups.php
+++ src/admin/MaintenanceGroups.php
@@ -1,34 +1,9 @@
 <?php
 require_once $GLOBALS ['basePath'] . 'src/ColumnFormatter.php';
 require_once $GLOBALS ['basePath'] . 'src/AutoForm.php';
+include_once ('FormatterColumnToCheckbox.php');
 
 // YAGNI: Include it in the ColumnFormatter class
-class FormatterColumnToCheckbox
-{
-
-
-	/**
-	 *
-	 * @param mixed $val
-	 * @param string $class
-	 * @return string
-	 */
-	public function getSpan ($val, $class)
-	{
-		$retVal = "<div class='$class'>";
-
-		$attributes = [ 'disabled'];
-		if ($val)
-		{
-			$attributes [] = 'checked';
-		}
-
-		$retVal .= '<input type="checkbox"' . join (' ', $attributes) . '>';
-		$retVal .= '</div>';
-		return $retVal;
-	}
-}
-
 class MaintenanceGroups extends Plugin
 {
 	private $jsonFile;
