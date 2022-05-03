da0c11870c14d1b2d5459075443eb5c898f1cfdf
Carlos Gomez <cgomez@grantiacapital.com>
2022-05-03 11:17:23 +0200

    Show users without group

--- src/admin/MaintenanceUsers.php
+++ src/admin/MaintenanceUsers.php
@@ -68,7 +68,7 @@ class MaintenanceUsers extends Plugin
 	{
 		// YAGNI: Receive an array with the necessary columns
 		$query = 'SELECT u.*,g.groups  FROM weUsers u ';
-		$query .= ' INNER JOIN  (SELECT GROUP_CONCAT(grpName  SEPARATOR ", ") AS groups, rel.idUser FROM weGroups g INNER JOIN weUsersGroups rel ON g.idGrp = rel.idGrp GROUP BY rel.idUser) g';
+		$query .= ' LEFT JOIN  (SELECT GROUP_CONCAT(grpName  SEPARATOR ", ") AS groups, rel.idUser FROM weGroups g INNER JOIN weUsersGroups rel ON g.idGrp = rel.idGrp GROUP BY rel.idUser) g';
 		$query .= ' ON u.idUser=g.idUser';
 		if ($idUser != 0)
 		{
