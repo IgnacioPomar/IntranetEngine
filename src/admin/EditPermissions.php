800a1680588d75a44a9049e7ae6fe7c09fc6defd
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2022-05-04 11:49:43 +0200

    Now we edit group pèrmissions

--- src/admin/EditPermissions.php
+++ src/admin/EditPermissions.php
@@ -9,7 +9,7 @@ class EditPermissions extends Plugin
 
 	private static function addPermCombo ($id, $defVal)
 	{
-		$retval = '<select id="' . $id . '">';
+		$retval = '<select name="' . $id . '">';
 		foreach (self::PERM_VALUES as $idc => $val)
 		{
 			$sel = '';
@@ -20,6 +20,88 @@ class EditPermissions extends Plugin
 		$retval .= "</select>";
 
 		return $retval;
+	}
+
+
+	private static function inputIdEncode ($node, $plg, $perm)
+	{
+		$id = rtrim (strtr (base64_encode ($node), '+/', '-_'), '=');
+		$id .= '@' . rtrim (strtr (base64_encode ($plg), '+/', '-_'), '=');
+		$id .= '@' . rtrim (strtr (base64_encode ($perm), '+/', '-_'), '=');
+
+		return 'b64@' . $id;
+	}
+
+
+	private static function inputIdDecode ($id)
+	{
+		$parts = explode ("@", $id);
+		$retVal = array ();
+		$retVal ['node'] = base64_decode (strtr ($parts [1], '-_', '+/'));
+		$retVal ['plg'] = base64_decode (strtr ($parts [2], '-_', '+/'));
+		$retVal ['perm'] = base64_decode (strtr ($parts [3], '-_', '+/'));
+
+		return $retVal;
+	}
+
+
+	private function saveGrpPerms ()
+	{
+		$sep = '';
+		$sql = 'INSERT INTO wePermissionsGroup (mnuNode,plgName,idGrp,permName, permValue) VALUES';
+		foreach ($_POST as $key => $val)
+		{
+			if (substr ($key, 0, 4) === 'b64@')
+			{
+
+				$id = self::inputIdDecode ($key);
+
+				$sql .= $sep . '(';
+				$sql .= '"' . $this->context->mysqli->real_escape_string ($id ['node']) . '",';
+				$sql .= '"' . $this->context->mysqli->real_escape_string ($id ['plg']) . '",';
+				$sql .= $_POST ['idGrp'] . ',';
+				$sql .= '"' . $this->context->mysqli->real_escape_string ($id ['perm']) . '",';
+				$sql .= $val . ')';
+
+				$sep = ',';
+			}
+		}
+		if ($sep == ',')
+		{
+			$sql .= 'ON DUPLICATE KEY UPDATE permValue=VALUES(permValue)';
+
+			$retVal = "<h1>Saving Permissions</h1>";
+			if (! $this->context->mysqli->query ($sql))
+			{
+				$retVal .= '<p class="error">Failed saving permissions.</p>';
+			}
+			else
+			{
+				$retVal .= '<p class="success">Permissions saved.</p>';
+			}
+
+			return $retVal . $this->showSelector ();
+		}
+
+		return '<p class="warning">There was no permmisions to save.</p>';
+	}
+
+
+	private function getStoredValue ($node, $plugin, $perm)
+	{
+		/*
+		 * if (! isset ($this->currentPerms [$node])) return 0;
+		 * $nodeGrp = &$this->currentPerms [$node];
+		 *
+		 * if (! isset ($nodeGrp [$plugin])) return 0;
+		 * $plgGrp = &$nodeGrp [$plugin];
+		 *
+		 * if (! isset ($plgGrp [$perm]))
+		 * return 0;
+		 * else
+		 * return $plgGrp [$perm];
+		 */
+		return $this->currentPerms [$node] [$plugin] [$perm] ?? 0;
 	}
 
 
@@ -37,14 +119,14 @@ class EditPermissions extends Plugin
 
 		foreach ($mnu as $opc)
 		{
-			$retVal .= '<div><span class="nodeName">' . $ident . $opc ['name'] . '</span>';
+			$retVal .= '<div class="menuNode"><span class="nodeName">' . $ident . $opc ['name'] . '</span>';
 
 			if ($this->isEditable)
 			{
 				if (isset ($opc ['opc']))
 				{
-					$defVal = $this->currentPerms [$mnu ['node']] [$mnu ['plg']] [''] ?? 0;
-					$id = $opc ['opc'] . '^' . $opc ['plg'] . '^';
+					$defVal = $this->getStoredValue ($opc ['opc'], $opc ['plg'], '');
+					$id = self::inputIdEncode ($opc ['opc'], $opc ['plg'], '');
 					$retVal .= self::addPermCombo ($id, $defVal);
 				}
 			}
@@ -55,15 +137,15 @@ class EditPermissions extends Plugin
 			// Aqui el combo selector
 			$retVal .= '</div>';
 
-			if (isset ($opc ['opc']) && isset ($this->plgssWithPerms [$opc [plg]]))
+			if (isset ($opc ['opc']) && isset ($this->plgssWithPerms [$opc ['plg']]))
 			{
-				$extraPerms = json_decode ($this->plgssWithPerms [$opc [plg]], true);
+				$extraPerms = json_decode ($this->plgssWithPerms [$opc ['plg']], true);
 				foreach ($extraPerms as $perm)
 				{
-					$retVal .= '<div><span class="nodeName">' . $ident . '<span class="pluginPerm">' . $perm . '</span></span>';
+					$retVal .= '<div class="pluginNode"><span class="nodeName">' . $ident . '<span class="pluginPerm">' . $perm . '</span></span>';
 
-					$defVal = $this->currentPerms [$mnu ['node']] [$mnu ['plg']] [$perm] ?? 0;
-					$id = $opc ['opc'] . '^' . $opc ['plg'] . '^' . $perm;
+					$defVal = $this->getStoredValue ($opc ['opc'], $opc ['plg'], $perm);
+					$id = self::inputIdEncode ($opc ['opc'], $opc ['plg'], $perm);
 					$retVal .= self::addPermCombo ($id, $defVal);
 					$retVal .= '</div>';
 				}
@@ -84,7 +166,7 @@ class EditPermissions extends Plugin
 	 *
 	 * @return string
 	 */
-	private function getMainMenu ($desc)
+	private function getMainMenu ($desc, $hiddenInput)
 	{
 		// Load the menu params
 		$this->isEditable = $this->context->mnu->isEditable;
@@ -93,10 +175,16 @@ class EditPermissions extends Plugin
 		$retVal = "<h1>Permissions Management for $desc</h1>";
 		if (! $this->isEditable)
 		{
-			$retVal .= '<p class"warning"> This is a fixed Menu. Is possible adjust params.</p>';
+			$retVal .= '<p class="info">This is a fixed menu, however it is possible to adjust the parameters.</p>';
 		}
 
+		$retVal .= '<form action="' . $this->uriPrefix . '" method="post" autocomplete="off">' . PHP_EOL;
+		$retVal .= $hiddenInput . PHP_EOL;
+
+		// YAGNI add the Menu editor HEader
 		$retVal .= '<div id="mnuEditor">' . $this->getMenuLevel (0, $this->context->mnu->arrOpcs) . '</div>';
+		$retVal .= '<button class="btn" type="submit" value="Save">Save</button>';
+		$retVal .= '</form>';
 
 		return $retVal;
 	}
@@ -152,7 +240,9 @@ class EditPermissions extends Plugin
 			}
 		}
 
-		return $this->getMainMenu ('group ' . $grpName);
+		$hiddenInput = '<input type="hidden" name="idGrp" value="' . $_GET ['idGrp'] . '" />';
+
+		return $this->getMainMenu ('group ' . $grpName, $hiddenInput);
 	}
 
 
@@ -225,6 +315,7 @@ class EditPermissions extends Plugin
 		}
 		else if (isset ($_POST ['idGrp']))
 		{
+			return $this->saveGrpPerms ();
 		}
 		else if (isset ($_POST ['idUsr']))
 		{
